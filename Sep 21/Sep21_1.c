13c2a1f2270aed45b839178a57896bca4b2e0b1d
bestware <49852633+ksh1t1z@users.noreply.github.com>
2020-09-22 15:21:59 +0545

    Update Sep21_1.c

--- Sep 21/Sep21_1.c
+++ Sep 21/Sep21_1.c
@@ -3,12 +3,12 @@ int main()
 {
     int a, b, c;
     b = 1;
-    c = 0;
+    c = 1;
     a = 0;
     printf("%d ", 0);
     for (int i = 1; i <= 20; ++i)
     {
-        printf("%d ", b);
+        printf("%d ", a);
         c = a + b;
         a = b;
         b = c;
